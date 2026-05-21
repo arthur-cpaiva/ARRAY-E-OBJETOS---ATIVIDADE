a936bbf9aa94525e2b38098445be8372eb9ebb77
Arthur Paiva <arthur-68@hotmail.com>
2026-05-21 20:38:22 -0300

    Lista de tarefas

---
+++ ARRAY PET/main.js
@@ -0,0 +1,17 @@
+const number = [6,8,10,12,16];
+
+console.log (number [0]);
+console.log (number [1]);
+console.log (number [2]);
+console.log (number [3]);
+console.log (number [4]);
+
+const pet = {
+    nome: 'Happy',
+    nasc: 2012,
+    raça: 'SRD',
+    especie: 'cachorro',
+    dono: 'Arthur',
+}
+
+alert ("Pet: " + pet.nome + "\nNascimento: " + pet.nasc + "\nRaça: " + pet.raça + "\nEspécie: " + pet.especie + "\nDono: " + pet.dono)
